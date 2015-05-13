883d7141b9e0e6fd0d5e50c5592968e106a650b0
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-13 17:07:36 -0400

    Got the apps.js file started

--- js/app.js
+++ js/app.js
@@ -4,3 +4,30 @@
 	// Your starting point. Enjoy the ride!
 
 })(window);
+
+
+
+document.head.parentElement.className = "js";
+
+var addTodo = document.querySelector('header');
+
+
+addTodo.addEventListener( '', function(){
+	console.log("Sup");
+})
+
+/*
+
+document.head.parentElement.className = "js";
+
+var elements =document.querySelectorAll('.cbp-nttrigger');
+
+
+
+_.forEach(elements, function(element, index, elements){
+  element.addEventListener('click', function(){
+    console.log(elements, index, element);
+    element.parentElement.classList.toggle('cbp-ntopen');
+});
+});
+*/
