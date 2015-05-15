d01709b9018d750daa613693f9ee0e2b44915279
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-05-15 16:37:33 -0400

    Refactored and added the li section
    @sjoyal @pgreglow @melrodbos @pope410211 @kimmattiko @jessyriordan @mstaehling @epicurean306 @gatorpazz

--- js/app.js
+++ js/app.js
@@ -175,7 +175,7 @@ document.head.parentElement.className = "js";
 var elements =document.querySelectorAll('.cbp-nttrigger');
 
 
-
+//
 _.forEach(elements, function(element, index, elements){
   element.addEventListener('click', function(){
     console.log(elements, index, element);
